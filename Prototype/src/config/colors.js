5c9777d68a508d7c5a8e3732e3a10bacc35bf971
Trevor Incze <trevor.incze@hotmail.com>
2020-11-08 16:06:24 -0500

    Added grey colour

--- Prototype/src/config/colors.js
+++ Prototype/src/config/colors.js
@@ -1,7 +1,9 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import React from "react";
 
 export default {
     primary: "#f65a5a",
     secondary: "#4ecdc4",
     black: "#000",
+    gray: "#666",
 };
