ba01bdc5f20043964bccd28cee04bd84f71d6b5b
Trevor Incze <trevor.incze@hotmail.com>
2020-11-08 16:04:37 -0500

    Cross-platform font now supports children prop

--- Prototype/src/components/AppText.js
+++ Prototype/src/components/AppText.js
@@ -1,24 +1,14 @@
 import React from "react";
-import { Text, StyleSheet, Platform } from "react-native";
+import { Text } from "react-native";
 
-function AppText({ children }) {
-    return <Text style={styles.text}>{children}</Text>;
+import defaultStyles from "../config/styles";
+
+function AppText({ children, style, ...otherProps }) {
+    return (
+        <Text style={[defaultStyles.text, style]} {...otherProps}>
+            {children}
+        </Text>
+    );
 }
-
-const styles = StyleSheet.create({
-    text: {
-        color: "tomato",
-        ...Platform.select({
-            ios: {
-                fontSize: 20,
-                fontFamily: "Avenir",
-            },
-            android: {
-                fontSize: 18,
-                fontFamily: "Roboto",
-            },
-        }),
-    },
-});
 
 export default AppText;
