b00278231aa62d8992a02bcec2b296762f20d378
Trevor Incze <trevor.incze@hotmail.com>
2020-11-07 23:28:11 -0500

    Fixed stylesheet syntax

--- Prototype/src/screens/LoginScreen.js
+++ Prototype/src/screens/LoginScreen.js
@@ -7,25 +7,19 @@ import colors from '../config/colors';
 export default class FormExample extends Component {
     render() {
         return (
-            <View style={{
-                flex: 1,
-                justifyContent: "center", // use "flex-end" later
-                alignItems: "center",
-                padding: 20,
-                marginVertical: 10,
-            }}>
-                <AppButton title="Login" color="primary"></AppButton>
-                <AppButton title="Register" color="secondary"></AppButton>
+            <View style={styles.container}>
+                <AppButton title="Login" color="primary"/>
+                <AppButton title="Register" color="secondary"/>
             </View>
         );
     }
 }
 
-/*const styles = StyleSheet.create ({
+const styles = StyleSheet.create ({
     container: {
         flex: 1,
         justifyContent: "center",
         alignItems: "center"
     }
-})*/
+})
 
