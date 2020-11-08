6d2f666ea25c27a6614d62e4e1426a25472992cb
Trevor Incze <trevor.incze@hotmail.com>
2020-11-07 19:00:41 -0500

    Modified component syntax to match others

--- Prototype/src/components/AppFooter.js
+++ Prototype/src/components/AppFooter.js
@@ -1,27 +1,26 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {Button, Footer, FooterTab, Icon, Text} from "native-base";
 
-class AppFooter extends Component {
-    render() {
-        return (
-            <Footer>
-                <FooterTab>
-                    <Button vertical>
-                        <Icon name="bookmarks" />
-                        <Text>Jobs</Text>
-                    </Button>
-                    <Button vertical active>
-                        <Icon active name="navigate" />
-                        <Text>Navigate</Text>
-                    </Button>
-                    <Button vertical>
-                        <Icon name="person" />
-                        <Text>Contact</Text>
-                    </Button>
-                </FooterTab>
-            </Footer>
-        );
-    }
+const AppFooter = (props) => {
+    return (
+        <Footer>
+            <FooterTab>
+
+                <Button vertical>
+                    <Icon name="bookmarks"/>
+                    <Text>Jobs</Text>
+                </Button>
+                <Button vertical active>
+                    <Icon active name="navigate"/>
+                    <Text>Navigate</Text>
+                </Button>
+                <Button vertical>
+                    <Icon name="person"/>
+                    <Text>Contact</Text>
+                </Button>
+            </FooterTab>
+        </Footer>
+    );
 }
 
 export default AppFooter;
