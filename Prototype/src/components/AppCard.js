90524196ff29fde360916c81e08ccc5a465d021e
Trevor Incze <trevor.incze@hotmail.com>
2020-11-07 19:11:15 -0500

    Modified component syntax to match others.

--- Prototype/src/components/AppCard.js
+++ Prototype/src/components/AppCard.js
@@ -17,59 +17,57 @@ import {
 const logo = require("../assets/logo.png");
 const cardImage = require("../assets/chair.jpg");
 
-class NHCardImage extends Component {
-    render() {
-        return (
-            <Container style={styles.container}>
+const AppCard = (props) => {
+    return (
+        <Container style={styles.container}>
 
 
-                <Content padder scrollEnabled={true}>
-                    <Card style={styles.mb}>
-                        <CardItem>
-                            <Left>
-                                <Thumbnail source={logo} />
-                                <Body>
-                                    <Text>Pool cleaning</Text>
-                                    <Text note>In 45 minutes</Text>
-                                </Body>
-                            </Left>
-                        </CardItem>
+            <Content padder scrollEnabled={true}>
+                <Card style={styles.mb}>
+                    <CardItem>
+                        <Left>
+                            <Thumbnail source={logo} />
+                            <Body>
+                                <Text>Pool cleaning</Text>
+                                <Text note>In 45 minutes</Text>
+                            </Body>
+                        </Left>
+                    </CardItem>
 
-                        <CardItem cardBody>
-                           <Text>Regular pool cleaning</Text>
-                            <Image
-                                style={{
-                                    resizeMode: "cover",
-                                    width: null,
-                                    height: 200,
-                                    flex: 1
-                                }}
-                                source={cardImage}
-                            />
-                        </CardItem>
+                    <CardItem cardBody>
+                       <Text>Regular pool cleaning</Text>
+                        <Image
+                            style={{
+                                resizeMode: "cover",
+                                width: null,
+                                height: 200,
+                                flex: 1
+                            }}
+                            source={cardImage}
+                        />
+                    </CardItem>
 
-                        <CardItem style={{ paddingVertical: 0 }}>
-                            <Left>
-                                <Button transparent>
-                                    <Icon active name="thumbs-up" />
-                                    <Text>23 Karens liked this</Text>
-                                </Button>
-                            </Left>
-                            {/*<Body>*/}
-                            {/*    <Button transparent>*/}
-                            {/*        <Icon active name="chatbubbles" />*/}
-                            {/*        <Text>89 Comments</Text>*/}
-                            {/*    </Button>*/}
-                            {/*</Body>*/}
-                            <Right>
-                                <Text>3:25PM</Text>
-                            </Right>
-                        </CardItem>
-                    </Card>
-                </Content>
-            </Container>
-        );
-    }
+                    <CardItem style={{ paddingVertical: 0 }}>
+                        <Left>
+                            <Button transparent>
+                                <Icon active name="thumbs-up" />
+                                <Text>23 Karens liked this</Text>
+                            </Button>
+                        </Left>
+                        {/*<Body>*/}
+                        {/*    <Button transparent>*/}
+                        {/*        <Icon active name="chatbubbles" />*/}
+                        {/*        <Text>89 Comments</Text>*/}
+                        {/*    </Button>*/}
+                        {/*</Body>*/}
+                        <Right>
+                            <Text>3:25PM</Text>
+                        </Right>
+                    </CardItem>
+                </Card>
+            </Content>
+        </Container>
+    );
 }
 
 const styles = StyleSheet.create({
@@ -81,4 +79,4 @@ const styles = StyleSheet.create({
     }
 });
 
-export default NHCardImage;
+export default AppCard;
