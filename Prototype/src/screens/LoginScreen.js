f1f5dccc8313ed14ac7dde1a1bf28cf9b902fae9
hitTheNoz <eamonlaughtoneagan@gmail.com>
2020-11-08 14:40:31 -0500

    added AppTextInput, Screen, LoginScreen, WelcomeScreen

--- Prototype/src/screens/LoginScreen.js
+++ Prototype/src/screens/LoginScreen.js
@@ -1,25 +1,49 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
+import {StyleSheet, ImageBackground, View, Image, Text} from "react-native";
 
-import { Container, Header, Content, Form, Item, Input, StyleSheet, View } from 'native-base';
+import Screen from '../components/Screen';
+import AppTextInput from '../components/AppTextInput';
 import AppButton from '../components/AppButton';
-import colors from '../config/colors';
 
-export default class FormExample extends Component {
-    render() {
-        return (
-            <View style={styles.container}>
-                <AppButton title="Login" color="primary"/>
-                <AppButton title="Register" color="secondary"/>
-            </View>
-        );
-    }
+function LoginScreen(props) {
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+    
+
+    return (
+        <Screen>
+            <Image 
+                style={styles.logo}
+                source={require('../assets/gear.png')}/>
+            <AppTextInput 
+                autoCapitalize="none"
+                autoCorrect={false}
+                icon="email"
+                keyboardType="email-address"
+                onChangeText={text => setEmail(text)}
+                placeholder="Email"
+            />
+            <AppTextInput 
+                autoCapitalize="none"
+                autoCorrect={false}
+                icon="lock"
+                onChangeText={text => setPassword(text)}
+                placeholdrer="Password"
+                secureTextEntry={true}
+            />
+            <AppButton title="Login" onPress={() => console.log(email, password)}/>
+        </Screen>
+    );
 }
 
-const styles = StyleSheet.create ({
-    container: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center"
+const styles = StyleSheet.create({
+    logo: {
+        width: 80,
+        height: 80,
+        alignSelf: 'center',
+        marginTop: 70,
+        marginBottom: 20,
     }
 })
 
+export default LoginScreen;
--- Prototype/src/screens/WelcomeScreen.js
+++ Prototype/src/screens/WelcomeScreen.js
@@ -1,47 +1,29 @@
-import React from 'react';
-import {StyleSheet, ImageBackground, View, Image, Text} from "react-native";
+import React, { Component } from 'react';
 
-function WelcomeScreen(props) {
-    return (
-        <ImageBackground
-            style={styles.background}
-            source={require("../assets/background.jpg")}
-        >
-            <View style={styles.logoContainer}>
-                <Image style={styles.logo} source={require("../assets/logo-red.png")}/>
-                <Text>Sell What You Don't Need</Text>
+import { Container, Header, Content, Form, Item, Input, StyleSheet, View } from 'native-base';
+import AppButton from '../components/AppButton';
+import colors from '../config/colors';
+
+export default class FormExample extends Component {
+    render() {
+        return (
+            <View style={{
+                    flex: 1,
+                    justifyContent: "center",
+                    alignItems: "center"}}>
+                        
+                <AppButton title="Login" color="primary"/>
+                <AppButton title="Register" color="secondary"/>
             </View>
-            <View style={styles.loginButton}/>
-            <View style={styles.registerButton}/>
-        </ImageBackground>
-    );
+        );
+    }
 }
 
-const styles = StyleSheet.create({
-    background: {
+/*const styles = StyleSheet.create ({
+    container: {
         flex: 1,
-        justifyContent: "flex-end",
+        justifyContent: "center",
         alignItems: "center"
-    },
-    logo: {
-      width: 100,
-      height: 100,
-    },
-    logoContainer: {
-        position: "absolute",
-        top: 70,
-        alignItems: "center"
-    },
-    loginButton: {
-        width: '100%',
-        height: 70,
-        backgroundColor: "#fc5c65"
-    },
-    registerButton: {
-        width: '100%',
-        height: 70,
-        backgroundColor: "#4ecdc4"
-    },
+    }
 })
-
-export default WelcomeScreen;
+*/
