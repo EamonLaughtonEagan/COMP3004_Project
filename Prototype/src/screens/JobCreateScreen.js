80e04ce8b36429e895a4daaed0d8a554c8fbba2e
hitTheNoz <eamonlaughtoneagan@gmail.com>
2020-12-01 21:19:30 -0500

    message go here

---
+++ Prototype/src/screens/JobCreateScreen.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {StyleSheet} from "react-native";
+
+import Screen from "../components/AppScreen";
+
+function JobCreateScreen({ route }) {
+    return (
+        <Screen>
+            
+        </Screen>
+    );
+}
+
+export default JobCreateScreen;
+
+const styles = StyleSheet.create({
+    
+})
