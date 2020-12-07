f2a07c872375e777835d168de2bdedcce1226b86
Trevor Incze <trevor.incze@hotmail.com>
2020-12-06 22:24:45 -0500

    Added reports parsing to GET /jobs route - all job get requests now include reports in jobs.

--- Server/routes/jobs.js
+++ Server/routes/jobs.js
@@ -3,30 +3,30 @@ const pool = require('../configs/dbConfig')
 const router = express.Router();
 
 const {formatCustomerJSON, formatSiteJSON, formatJobJSON,
-	formatJobFullJSON} = require("../configs/jsonConfig")
+	formatJobFullJSON, formatReportsJSON} = require("../configs/jsonConfig")
 
 
-// Handle job GET route for all jobs
+// job GET route for querying all jobs
 router.get('/', (req, res) => {
-	const query = `SELECT
+	const jobsQuery = `SELECT
 		j.job_id, j.name, j.description, j.start_time, j.end_time,
 		c.customer_id, c.first_name, c.last_name, c.email, c.cell, c.home,
-		s.site_id, s.address, s.volume, s.type
+		s.site_id, s.address, s.volume, s.type,
+		r.report_id, r.status_id, r.user_id, r.text 
 		FROM jobs AS j INNER JOIN customers AS c ON c.customer_id = j.customer_id
-		INNER JOIN sites AS s ON s.site_id = j.site_id`
+		INNER JOIN sites AS s ON s.site_id = j.site_id
+		LEFT OUTER JOIN reports AS r ON r.job_id = j.job_id
+		ORDER BY job_id
+		`
 
-	pool.query(query, (err, results, fields) => {
+	pool.query(jobsQuery, (err, results, fields) => {
 		if (err) {
 			const response = { data: null, message: err.message, }
-			res.send(response);
+			res.status(500).send(response);
 			return;
 		}
 
-		let jobs = [];
-
-		results.forEach(e => {
-			jobs.push(formatJobFullJSON(e));
-		});
+		let jobs = parseJobs(results);
 
 		res.status(200).send({
 			data: jobs,
@@ -35,32 +35,35 @@ router.get('/', (req, res) => {
 	});
 });
 
-// Handle job GET route for specific job
+// job GET route for querying a specific job
 router.get('/:id', (req, res) => {
 	const id = req.params.id
+
 	const query = `SELECT
 		j.job_id, j.name, j.description, j.start_time, j.end_time,
 		c.customer_id, c.first_name, c.last_name, c.email, c.cell, c.home,
-		s.site_id, s.address, s.volume, s.type
+		s.site_id, s.address, s.volume, s.type,
+		r.report_id, r.status_id, r.user_id, r.text
 		FROM jobs AS j INNER JOIN customers AS c ON c.customer_id = j.customer_id
 		INNER JOIN sites AS s ON s.site_id = j.site_id
-		WHERE job.job_id = '${id}'
-		LIMIT 1`;
-
-	//    const sql = `SELECT * FROM sites WHERE site_id='${site_id}' LIMIT 1`
+		LEFT OUTER JOIN reports AS r ON r.job_id = j.job_id
+		WHERE j.job_id = '${id}'`;
 
 	pool.query(query, (err, results, fields) => {
 		if (err) {
 			const response = { data: null, message: err.message, }
-			res.send(response);
+			res.status(500).send(response);
+			return;
 		}
 
 		let response;
 		if (results) {
-			const data = formatJobFullJSON(results[0]);
+			// Only one job is being queried; send it without the array
+			let jobObject = parseJobs(results)[0];
+
 			res.status(200).send({
-				data: data,
-				message: `Job ${data.job.job_id} successfully retrieved.`,
+				data: jobObject,
+				message: `Job ${jobObject.job.job_id} successfully retrieved.`,
 			})
 		} else {
 			res.status(400).send({data: null, message: "Job " + id + " does not exist"});
@@ -68,7 +71,7 @@ router.get('/:id', (req, res) => {
 	});
 });
 
-// Create a new job - POST /jobs/
+// job POST route for creating a new job
 router.post('/', (req, res) => {
 	if (!req.is('application/json')) {
 		res.status(400).send({ data: null, message: "Content-type must be application/json" });
@@ -122,7 +125,7 @@ router.post('/', (req, res) => {
 
 });
 
-// Handle job PUT route - Update an existing job
+// job PUT route for updating an existing job
 router.put('/:id', (req, res) => {
 	if (!req.is('application/json')) {
 		const response = { data: null, message: "Content-type must be application/json" };
@@ -177,7 +180,7 @@ router.put('/:id', (req, res) => {
 	});
 });
 
-// Handle job DELETE route - Delete an existing job
+// job DELETE route for deleting an existing job
 router.delete('/:id', (req, res) => {
 	const { id } = req.params;
 	const query = `DELETE FROM jobs WHERE job_id=${id}`;
@@ -196,11 +199,92 @@ router.delete('/:id', (req, res) => {
 	});
 });
 
-// Handle invalid /jobs/ route
+// invalid /jobs/ route
 router.all('/*', function(req, res) {
 	res.status(400).send({
 		data: null, message: 'Route not found'
 	});
 });
+
+function parseJobs(results) {
+	/* Parse messy query results into a structured object.
+	* This is used in job GET requests (/jobs/ and /jobs{id}
+	*/
+
+	let jobs = [];
+	let prevIndex = -1;
+	results.forEach((e, index) => {
+		/*
+		Querying jobs is complicated because their information is spread amongst many tables:
+		Jobs relate to 1 customer, 1 site, 0 or many reports.
+
+		If a job has multiple reports, queries return multiple rows for the same job.
+            Ex:
+                job_id		report_id	report_text
+                    1			1		Customer was not home; they will need to reschedule pool school
+                    1			2		The pool pump was installed without issues
+
+		This function parses these rows into a structured object. Example output below:
+		[
+			{
+			  "job": {
+				"job_id": 1,
+				"site_id": 1,
+				"customer_id": 1,
+				"name": "Pool cleaning + deliver order",
+				"description": "Clean the pool and deliver the chlorine that Joe ordered",
+				"start_time": "2020-11-30T02:51:37.000Z",
+				"end_time": "2020-11-30T04:51:37.000Z",
+				"reports": [
+				  {
+					"report_id": 4,
+					"user_id": 2,
+					"job_id": 1,
+					"status_id": 2,
+					"text": "Customer requested a new jet faceplate be delivered"
+				  },
+				  {
+					"report_id": 3,
+					"user_id": 2,
+					"job_id": 1,
+					"status_id": 1,
+					"text": "No problems with scheduled job"
+				  }
+				]
+			  },
+			  "customer": {
+				"customer_id": 1,
+				"site_id": 1,
+				"first_name": "joe",
+				"last_name": "blow",
+				"email": "joe.blow@gmail.com",
+				"cell": "555 432-1987",
+				"home": "555 843-4812"
+			  },
+			  "site": {
+				"site_id": 1,
+				"customer_id": 1,
+				"address": "123 Smiley St.",
+				"volume": 50000,
+				"type": "Inground pool"
+			  }
+			},
+
+			{
+			... (more jobs here)
+			}
+		  ]
+        */
+
+		if (prevIndex > -1 && jobs[prevIndex].job.job_id === e.job_id) {
+			jobs[prevIndex].job.reports.push(formatReportsJSON(e));
+		} else {
+			jobs.push(formatJobFullJSON(e));
+			prevIndex++;
+		}
+	});
+
+	return jobs;
+}
 
 module.exports = router;
