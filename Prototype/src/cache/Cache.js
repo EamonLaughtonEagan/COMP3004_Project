9092b920c1f78bd11bcd062cd9f55c532846c711
Trevor Incze <trevor.incze@hotmail.com>
2020-12-07 12:17:33 -0500

    Grouped functions into classes, so functions can be imported in groups.
    Before:
    import {fetchUsers, getUsers, findUser, fetchCustomers, getCustomers, findCustomer } from "../cache/Cache"
    
    After:
    import {Users, Customers} from "../cache/Cache"

--- Prototype/src/cache/Cache.js
+++ Prototype/src/cache/Cache.js
@@ -8,59 +8,92 @@ let jobs = [];
 let customers = [];
 let users = [];
 
-const fetchJobs = async () => {
-    try {
-        const response = await fetch(endpointJobs);
-        const json = await response.json();
-
-        jobs = json.data;
-        return jobs;
-    } catch (error) {
-        console.error(error);
-    }
+// TODO: Initialize this data on app startup asynchronously
+const initialize = async () => {
+    await Users.fetchUsers();
+    await Customers.fetchCustomers();
 };
 
-const fetchCustomers = async () => {
-    try {
-        const response = await fetch(endpointCustomers);
-        const json = await response.json();
+export class Users {
+    static fetchUsers = async () => {
+        try {
+            const response = await fetch(endpointUsers);
+            const json = await response.json();
 
-        customers = json.data;
-        return customers;
-    } catch (error) {
-        console.error("Error while fetching customers: " + error.message);
-    }
-};
+            users = json.data;
+            return users;
+        } catch (error) {
+            console.error("Error while fetching users: " + error.message);
+        }
+    };
 
-const fetchUsers = async () => {
-    try {
-        const response = await fetch(endpointUsers);
-        const json = await response.json();
+    static findUser = (user_id) => {
+        users.forEach((u) => {
+            if (u.user_id === user_id) {
+                return u;
+            }
+        });
 
-        users = json.data;
+        return null;
+    };
+
+    static getUsers = () => {
         return users;
-    } catch (error) {
-        console.error("Error while fetching users: " + error.message);
-    }
-};
+    };
+}
 
-const getJobs = () => {
-    return jobs;
-};
+export class Jobs {
+    static getJob = (job_id) => {
+        jobs.forEach((j) => {
+            if (j.job_id === job_id) {
+                return j;
+            }
+        });
 
-const getCustomers = () => {
-    return customers;
-};
+        return null;
+    };
 
-const getUsers = () => {
-    return users;
-};
+    static fetchJobs = async () => {
+        try {
+            const response = await fetch(endpointJobs);
+            const json = await response.json();
 
-module.exports = {
-    fetchJobs,
-    fetchCustomers,
-    fetchUsers,
-    getJobs,
-    getCustomers,
-    getUsers,
+            jobs = json.data;
+            return jobs;
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    static getJobs = () => {
+        return jobs;
+    };
+}
+
+export class Customers {
+    static getCustomer = (customer_id) => {
+        customers.forEach((c) => {
+            if (c.customer_id === customer_id) {
+                return c;
+            }
+        });
+
+        return null;
+    };
+
+    static fetchCustomers = async () => {
+        try {
+            const response = await fetch(endpointCustomers);
+            const json = await response.json();
+
+            customers = json.data;
+            return customers;
+        } catch (error) {
+            console.error("Error while fetching customers: " + error.message);
+        }
+    };
+
+    static getCustomers = () => {
+        return customers;
+    };
 }
