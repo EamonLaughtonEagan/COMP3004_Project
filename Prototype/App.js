ec515db2bc1af8385150af85c8411b1540563e8f
Trevor Incze <trevor.incze@hotmail.com>
2020-12-06 22:23:29 -0500

    Moved NavigationContainer wrapper to WelcomeNavigator.js. Removed unused imports

--- Prototype/App.js
+++ Prototype/App.js
@@ -1,31 +1,22 @@
 import "react-native-gesture-handler";
-import NavigationContainer from "@react-navigation/native/src/NavigationContainer";
-import { createStackNavigator } from "@react-navigation/stack";
-import React, { useState } from "react";
+import React from "react";
 
-import HomeNavigator from "./src/navigation/HomeNavigator";
 import WelcomeNavigator from "./src/navigation/WelcomeNavigator";
-import routes from "./src/navigation/routes";
-import JobListScreen from "./src/screens/JobListScreen";
 
-const Stack = createStackNavigator();
-const StackNavigator = () => (
-    <NavigationContainer>
-        <Stack.Navigator>
-            <Stack.Screen
-                name={routes.LOGIN}
-                component={LoginScreen}
-                options={{ title: "Login", headerShown: false }}
-            />
-            <Stack.Screen name={routes.JOB_LIST} component={JobListScreen} />
-        </Stack.Navigator>
-    </NavigationContainer>
-);
+//const Stack = createStackNavigator();
+// const StackNavigator = () => (
+//     <NavigationContainer>
+//         <Stack.Navigator>
+//             <Stack.Screen
+//                 name={routes.LOGIN}
+//                 component={LoginScreen}
+//                 options={{ title: "Login", headerShown: false }}
+//             />
+//             <Stack.Screen name={routes.JOB_LIST} component={JobListScreen} />
+//         </Stack.Navigator>
+//     </NavigationContainer>
+// );
 
 export default function App() {
-    return (
-        <NavigationContainer>
-            <WelcomeNavigator />
-        </NavigationContainer>
-    );
+    return <WelcomeNavigator />;
 }
--- Prototype/src/navigation/WelcomeNavigator.js
+++ Prototype/src/navigation/WelcomeNavigator.js
@@ -1,7 +1,7 @@
+import NavigationContainer from "@react-navigation/native/src/NavigationContainer";
 import { createStackNavigator } from "@react-navigation/stack";
 import React from "react";
 
-import JobListScreen from "../screens/JobListScreen";
 import LoginScreen from "../screens/LoginScreen";
 import WelcomeScreen from "../screens/WelcomeScreen";
 import HomeNavigator from "./HomeNavigator";
@@ -10,15 +10,17 @@ import routes from "./routes";
 const Stack = createStackNavigator();
 
 const WelcomeNavigator = () => (
-    <Stack.Navigator
-        screenOptions={{
-            headerShown: false,
-        }}
-    >
-        <Stack.Screen name={routes.WELCOME} component={WelcomeScreen} />
-        <Stack.Screen name={routes.LOGIN} component={LoginScreen} />
-        <Stack.Screen name={routes.HOME} component={HomeNavigator} />
-    </Stack.Navigator>
+    <NavigationContainer>
+        <Stack.Navigator
+            screenOptions={{
+                headerShown: false,
+            }}
+        >
+            <Stack.Screen name={routes.WELCOME} component={WelcomeScreen} />
+            <Stack.Screen name={routes.LOGIN} component={LoginScreen} />
+            <Stack.Screen name={routes.HOME} component={HomeNavigator} />
+        </Stack.Navigator>
+    </NavigationContainer>
 );
 
 export default WelcomeNavigator;
