c9633b7a5e65eeeaf9d9219c9c9fb649ffb63dbf
Trevor Incze <trevor.incze@hotmail.com>
2020-12-07 16:59:15 -0500

    - Finished asynchronous fetching calls (resolved promises now return in fetch() calls)
    -Added Cache initialization with load times

--- Prototype/src/cache/Cache.js
+++ Prototype/src/cache/Cache.js
@@ -4,31 +4,102 @@ const endpointJobs = endpoint + "jobs/";
 const endpointUsers = endpoint + "users/";
 const endpointCustomers = endpoint + "customers/";
 
-let jobs = [];
-let customers = [];
-let users = [];
+const jsonType = "application/json";
 
-// TODO: Initialize this data on app startup asynchronously
-const initialize = async () => {
-    await Users.fetchUsers();
-    await Customers.fetchCustomers();
-};
+export class Cache {
+    static initialized = false;
+
+    static jobs = [];
+    static customers = [];
+    static users = [];
+
+    static initialize = () => {
+        console.log("Initializing cache...");
+        const userTime = Date.now();
+
+        Users.fetchUsers().then((r) => {
+            console.log(
+                "Fetched " +
+                    Cache.users.length +
+                    " users (" +
+                    (Date.now() - userTime) +
+                    "ms)"
+            );
+        });
+
+        const customerTime = Date.now();
+        Customers.fetchCustomers().then((r) => {
+            console.log(
+                "Fetched " +
+                    Cache.customers.length +
+                    " customers (" +
+                    (Date.now() - customerTime) +
+                    "ms)"
+            );
+        });
+
+        const jobsTime = Date.now();
+        Jobs.fetchJobs().then((r) => {
+            console.log(
+                "Fetched " +
+                    Cache.jobs.length +
+                    " jobs (" +
+                    (Date.now() - jobsTime) +
+                    "ms)"
+            );
+        });
+    };
+
+    static createPostJSON = (obj) => {
+        return {
+            headers: {
+                "Content-Type": "application/json",
+            },
+            method: "POST",
+            body: JSON.stringify(obj),
+        };
+    };
+
+    static validateJSONResponse = (res) => {
+        if (!res.ok) {
+            throw new Error("Server error during POST request: " + res.message);
+        }
+
+        const contentType = res.headers.get("Content-Type");
+        if (contentType !== jsonType) {
+            throw new Error(
+                "Unexpected Content-Type " +
+                    contentType +
+                    " in POST request response."
+            );
+        }
+
+        return res;
+    };
+}
 
 export class Users {
-    static fetchUsers = async () => {
-        try {
-            const response = await fetch(endpointUsers);
-            const json = await response.json();
-
-            users = json.data;
-            return users;
-        } catch (error) {
-            console.error("Error while fetching users: " + error.message);
-        }
+    /**
+     * Fetch users from the server asynchronously.
+     * @return If successful, a resolved Promise. Otherwise, an error is thrown.
+     */
+    static fetchUsers = () => {
+        return fetch(endpointUsers)
+            .then((response) => {
+                return response.json();
+            })
+            .then((json) => {
+                Cache.users = json.data;
+                return Promise.resolve();
+            })
+            .catch((err) => {
+                console.log("Error while fetching users: " + err.message);
+                throw err;
+            });
     };
 
     static findUser = (user_id) => {
-        users.forEach((u) => {
+        Cache.users.forEach((u) => {
             if (u.user_id === user_id) {
                 return u;
             }
@@ -38,13 +109,32 @@ export class Users {
     };
 
     static getUsers = () => {
-        return users;
+        return Cache.users;
     };
 }
 
 export class Jobs {
+    /**
+     * Fetch jobs from the server asynchronously
+     * @return If successful, a resolved Promise. Otherwise, an error is thrown.
+     */
+    static fetchJobs = () => {
+        return fetch(endpointJobs)
+            .then((response) => {
+                return response.json();
+            })
+            .then((json) => {
+                Cache.jobs = json.data;
+                return Promise.resolve();
+            })
+            .catch((err) => {
+                console.log("Error while fetching jobs: " + err.message);
+                throw err;
+            });
+    };
+
     static getJob = (job_id) => {
-        jobs.forEach((j) => {
+        Cache.jobs.forEach((j) => {
             if (j.job_id === job_id) {
                 return j;
             }
@@ -53,26 +143,54 @@ export class Jobs {
         return null;
     };
 
-    static fetchJobs = async () => {
-        try {
-            const response = await fetch(endpointJobs);
-            const json = await response.json();
-
-            jobs = json.data;
-            return jobs;
-        } catch (error) {
-            console.error(error);
-        }
+    static getJobs = () => {
+        return Cache.jobs;
     };
 
-    static getJobs = () => {
-        return jobs;
+    static postJob = (jobObject) => {
+        if (!jobObject || !jobObject.job || !jobObject.customer) {
+            throw new Error(
+                "Malformed job post. jobObject must contain at least .job and .customer"
+            );
+        }
+
+        const resJSON = fetch(
+            endpointJobs + "/" + jobObject.job.job_id,
+            Cache.createPostJSON(jobObject)
+        )
+            .then((response) => {
+                return Cache.validateJSONResponse(response).json();
+            })
+            .catch((error) => {
+                throw error;
+            });
+
+        console.log("Response from server: " + JSON.stringify(resJSON));
     };
 }
 
 export class Customers {
+    /**
+     * Fetch users from the server. Blocks thread while waiting for response.
+     * @return If successful, a resolved Promise. Otherwise, an error is thrown.
+     */
+    static fetchCustomers = () => {
+        return fetch(endpointCustomers)
+            .then((response) => {
+                return response.json();
+            })
+            .then((json) => {
+                Cache.customers = json.data;
+                return Promise.resolve();
+            })
+            .catch((err) => {
+                console.log("Error while fetching customers: " + err.message);
+                throw err;
+            });
+    };
+
     static getCustomer = (customer_id) => {
-        customers.forEach((c) => {
+        Cache.customers.forEach((c) => {
             if (c.customer_id === customer_id) {
                 return c;
             }
@@ -81,19 +199,13 @@ export class Customers {
         return null;
     };
 
-    static fetchCustomers = async () => {
-        try {
-            const response = await fetch(endpointCustomers);
-            const json = await response.json();
-
-            customers = json.data;
-            return customers;
-        } catch (error) {
-            console.error("Error while fetching customers: " + error.message);
-        }
-    };
+    static findCustomer(site_id) {
+        return Cache.customers.find((customer) => {
+            customer.sites.some((site) => site.site_id === site_id);
+        });
+    }
 
     static getCustomers = () => {
-        return customers;
+        return Cache.customers;
     };
 }
