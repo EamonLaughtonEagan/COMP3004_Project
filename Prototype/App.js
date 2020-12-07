52d04706ced43818f3b9e42f794febf6180ba2ab
Trevor Incze <trevor.incze@hotmail.com>
2020-12-07 12:06:03 -0500

    Moved NavigationContainer wrapper to WelcomeNavigator.js

--- Prototype/App.js
+++ Prototype/App.js
@@ -1,7 +1,10 @@
 import "react-native-gesture-handler";
 import React from "react";
 
+import { ReportList } from "./src/job/ReportList";
 import WelcomeNavigator from "./src/navigation/WelcomeNavigator";
+import Screen from "./src/components/Screen";
+import {Text} from "react-native";
 
 //const Stack = createStackNavigator();
 // const StackNavigator = () => (
@@ -17,6 +20,28 @@ import WelcomeNavigator from "./src/navigation/WelcomeNavigator";
 //     </NavigationContainer>
 // );
 
+const dummyReports = [
+    {
+        report_id: 4,
+        user_id: 2,
+        job_id: 1,
+        status_id: 2,
+        text: "Dummy report text here",
+    },
+    {
+        report_id: 5,
+        user_id: 2,
+        job_id: 1,
+        status_id: 1,
+        text: "Other dummy report text",
+    },
+];
+
 export default function App() {
+ //    return (
+ //        <Screen>
+ //            <ReportList reports={dummyReports} />
+ //        </Screen>
+ //    );
     return <WelcomeNavigator />;
 }
