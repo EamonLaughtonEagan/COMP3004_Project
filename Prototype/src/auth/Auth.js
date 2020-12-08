e9cbeb578eb8a5663599b271253dcc25d1e32d24
Trevor Incze <trevor.incze@hotmail.com>
2020-12-08 15:57:27 -0500

    Removed callback for fetching new users; instead users should be re-cached when new users are created.

--- Prototype/src/auth/Auth.js
+++ Prototype/src/auth/Auth.js
@@ -8,29 +8,21 @@ export class Auth {
             return null;
         }
 
-        const authCallback = () => {
-            Cache.users.forEach((u) => {
-                if (u.email.toLowerCase() === email.toLowerCase()) {
-                    Auth.user = u;
-                }
-            });
-
-            if (!Auth.user) {
-                console.log("No user with email " + email + " found");
-                return null;
+        Cache.users.forEach((u) => {
+            if (u.email.toLowerCase() === email.toLowerCase()) {
+                Auth.user = u;
             }
+        });
 
-            if (pass === "hunter2") {
-                return Auth.user;
-            }
-        };
-
-        if (!Cache.fetchedRecently()) {
-            Users.fetchUsers().then(() => {
-                authCallback();
-            });
-        } else {
-            authCallback();
+        if (!Auth.user) {
+            console.log("No user with email " + email + " found");
+            return null;
         }
+
+        if (pass === "hunter2") {
+            return Auth.user;
+        }
+
+        return Auth.user;
     };
 }
