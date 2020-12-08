b15343c036b9ddd38a8c71e73e45f1e608aad0d0
Trevor Incze <trevor.incze@hotmail.com>
2020-12-08 16:12:59 -0500

    Renamed Cache.initialize() to Cache.fetchAll() and removed old code comments

--- Prototype/App.js
+++ Prototype/App.js
@@ -1,32 +1,12 @@
 import "react-native-gesture-handler";
 import React from "react";
 
-import { Cache, Customers } from "./src/cache/Cache";
+import { Cache} from "./src/cache/Cache";
 import WelcomeNavigator from "./src/navigation/WelcomeNavigator";
 
-//const Stack = createStackNavigator();
-// const StackNavigator = () => (
-//     <NavigationContainer>
-//         <Stack.Navigator>
-//             <Stack.Screen
-//                 name={routes.LOGIN}
-//                 component={LoginScreen}
-//                 options={{ title: "Login", headerShown: false }}
-//             />
-//             <Stack.Screen name={routes.JOB_LIST} component={JobListScreen} />
-//         </Stack.Navigator>
-//     </NavigationContainer>
-// );
-
 export default function App() {
-    //    return (
-    //        <Screen>
-    //            <ReportList reports={dummyReports} />
-    //        </Screen>
-    //    );
     console.log("Initializing app...");
-    Cache.initialize();
-
+    Cache.fetchAll();
     console.log("Rendering app...");
 
     return <WelcomeNavigator />;
