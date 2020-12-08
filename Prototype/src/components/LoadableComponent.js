2447f17194159e7f960fed29d54fc57b6b2c1c64
Trevor Incze <trevor.incze@hotmail.com>
2020-12-08 16:07:47 -0500

    Created LoadableComponent.js
    Loads on component mount. Subclasses should implement load(), which captures errors and updates this.loaded, this.error appropriately.
    
    useState is avoided because it can't be used before the component is mounted.

---
+++ Prototype/src/components/LoadableComponent.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+/**
+ * A component that requires loading. Requirements:
+ *      - props.loadCallback must return a Promise
+ *      - render() in child component must handle "loaded" and "error" states
+ */
+export class LoadableComponent extends React.Component {
+
+    // useState can't be used while the component is mounting, so these variables are used instead
+    loaded = false;
+    error = false;
+
+    load = (force) => {
+        throw new Error("load() must be implemented in subclass");
+    };
+
+    reload = (force = false) => {
+        if (force || (!this.loaded && !this.error)) {
+            try {
+                this.load();
+                this.loaded = true;
+                this.error = false;
+
+                this.forceUpdate();
+                return true;
+            } catch (err) {
+                this.error = true;
+                this.loaded = false;
+                return false;
+            }
+        }
+    };
+
+    async componentDidMount() {
+        return this.reload();
+    }
+}
