643c79e9a1e8bdf2bdeb358877844586629b7112
Trevor Incze <trevor.incze@hotmail.com>
2020-12-09 00:37:50 -0500

    Started work on Header for all screens. The button will eventually open the Drawer Navigator so that swiping isn't necessary.

--- Prototype/src/components/Screen.js
+++ Prototype/src/components/Screen.js
@@ -1,10 +1,41 @@
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 import Constants from "expo-constants";
 import React from "react";
-import { StyleSheet, SafeAreaView, View } from "react-native";
+import { StyleSheet, SafeAreaView, View, Text } from "react-native";
 
-function Screen({ children, style }) {
+import common from "../config/common";
+
+function Screen({ children, style, title, hideHeader, navigation }) {
+    const displayTitle = style || "missing title";
     return (
         <SafeAreaView style={[styles.screen, style]}>
+            <View
+                style={{
+                    height: 50,
+                    borderBottomWidth: 1,
+                    borderBottomColor: common.softGray,
+                    padding: 15,
+                    flexDirection: "column",
+                }}
+            >
+                <MaterialCommunityIcons
+                    name="menu"
+                    size={24}
+                    color="black"
+                    onPress={() => {
+                        if (navigation) {
+                            navigation.openDrawer();
+                        }
+                    }}
+                />
+                <Text
+                    style={{
+                        alignSelf: "center",
+                    }}
+                >
+                    {displayTitle}
+                </Text>
+            </View>
             <View style={style}>{children}</View>
         </SafeAreaView>
     );
